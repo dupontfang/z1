26b6beae52b7f504f48eeb74786fa60dab4c7c53
wf <weiyun.fang@gmail.com>
2020-12-21 09:54:55 +1100

    Init Commit

---
+++ com.test1/src/main/java/com/weiyun/group/com/test1/App.java
@@ -0,0 +1,13 @@
+package com.weiyun.group.com.test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
